07495dda3a1e63701a81c44092c614b11343ec22
Jon Borgonia <jon@gomagames.com>
2015-10-19 00:37:01 -1000

    adds price estimate detail
    adds price detail template
    adds skeleton css

--- public/js/views/price.js
+++ public/js/views/price.js
@@ -1,8 +1,31 @@
 'use strict';
 
+var PriceView = Backbone.View.extend({
+  el: $('#price-detail'),
+  template: 'price',
+  render: function(){
+    var self = this;
+    TemplateManager.get(this.template, function(template){
+      var html = Handlebars.compile(template)(self.model.toJSON());
+      self.$el.html(html);
+    });
+  }
+});
+
 var PricesView = Backbone.View.extend({
   el: $('#prices'),
   template: 'prices',
+  events: {
+    'click li' : 'renderPriceDetail'
+  },
+  renderPriceDetail : function(event){
+    var idx = $(event.currentTarget).data('id');
+    var price = this.collection.models[idx];
+    var priceView = new PriceView({
+      model : price
+    });
+    priceView.render();
+  },
   render: function(){
     var self = this;
     TemplateManager.get(this.template, function(template){
