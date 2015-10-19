511d4701c22171f8af2950edf76a39a7f52c10b3
Jon Borgonia <jon@gomagames.com>
2015-10-18 18:01:55 -1000

    adds static dir

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.end("Hello world");
-});
+app.use(express.static('./public'));
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server is listening on port', process.env.PORT || 3000);
